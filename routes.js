10395d0914d5697e8abba4d0bc0d5f9778db9bea
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-06 22:15:09 -0300

    Cadastro de instrutores

--- routes.js
+++ routes.js
@@ -9,8 +9,20 @@ routes.get('/login', (req, res) => {
     return res.render('login');
 });
 
+routes.post('/login', (req, res) => {
+    return res.render('login');
+});
+
 routes.get('/teachers', (req, res) => {
     return res.render('teachers/index');
+});
+
+routes.get('/teachers/create', (req, res) => {
+    return res.render('teachers/create');
+});
+
+routes.post('/teachers/create', (req, res) => {
+    return res.render('teachers/create');
 });
 
 routes.get('/students', (req, res) => {
