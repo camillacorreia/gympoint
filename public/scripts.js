d3e447455f35c4bd4c0c70ab1f17d5afe8e919f3
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-09 00:47:28 -0300

    Confirmação e exclusão de alunos

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,11 @@ for (item of menuItens) {
         item.classList.add("active");
     }
 }
+
+const formDelete = document.querySelector("#form-delete");
+formDelete.addEventListener("submit", function(event) {
+    const confirmation = confirm("Deseja deletar?")
+    if(!confirmation) {
+        event.preventDefault()
+    }
+});
