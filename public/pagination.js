4305b32bd774fe4f3e59218da5ad0a119aa93c0e
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-15 19:50:19 -0300

    Alguns ajustes na paginação

---
+++ public/pagination.js
@@ -0,0 +1,46 @@
+function paginate(selectedPage, totalPages) {
+
+    let pages = [],
+        oldPage
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+        if(firstAndLastPage || pagesAfterSelectedPage && pagesBeforeSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...");
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1);
+            }
+
+            pages.push(currentPage);
+
+            oldPage = currentPage;
+        }
+    }
+
+    return pages
+}
+
+const pagination = document.querySelector(".pagination");
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total);
+
+let elements = "";
+
+for (let page of pages) {
+
+    if(String(page).includes("...")) {
+        elements += `<span>${page}</span>`;
+    } else {
+        elements += `<a href="?page=${page}">${page}</a>`;
+    }
+}
+
+pagination.innerHTML= elements;
