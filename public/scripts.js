a449c3192f58c24dc15cc05278a6ca61dbbc1514
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-15 18:33:25 -0300

    Algoritmo de paginação no front

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,32 @@ formDelete.addEventListener("submit", function(event) {
         event.preventDefault()
     }
 });
+
+//Paginação
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+    if(firstAndLastPage || pagesAfterSelectedPage && pagesBeforeSelectedPage) {
+        if (oldPage && currentPage - oldPage > 2) {
+            pages.push("...");
+        }
+
+        if (oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1);
+        }
+
+        pages.push(currentPage);
+
+        oldPage = currentPage;
+    }
+}
+
+console.log(pages)
